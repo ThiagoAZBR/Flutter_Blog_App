be661156d0b97177d4b19523831fda99b4fa5ad7
Thiago A Zeferino <thiagoaugustozeferino@gmail.com>
2022-03-30 23:27:05 -0300

    :fire: remove: useless code

--- lib/app/domain/usecases/search_articles.dart
+++ lib/app/domain/usecases/search_articles.dart
@@ -2,8 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_blog_app/app/domain/entities/ny_times_search.dart';
 import 'package:flutter_blog_app/app/domain/repositories/search_articles_repository.dart';
 import 'package:flutter_blog_app/app/domain/usecases/usecase.dart';
-import 'package:flutter_blog_app/app/shared/errors/error_messages.dart';
-import 'package:flutter_blog_app/app/shared/errors/errors.dart';
 
 class SearchArticleUseCase
     implements UseCase<NYTimesSearch, SearchArticleParams> {
@@ -15,11 +13,7 @@ class SearchArticleUseCase
   @override
   Future<Either<Exception, NYTimesSearch>> call(
       SearchArticleParams params) async {
-    try {
-      return await _searchArticleRepository.searchArticles(params);
-    } catch (e) {
-      return Left(SearchFailed(ErrorMessages.wrongSubject));
-    }
+    return await _searchArticleRepository.searchArticles(params);
   }
 }
 
